119c89f09ca300c07c95796d19b07656d4cd95f8
RameshKrishnanNaraKrish <nkrk1996@outlook.com>
2024-08-19 13:37:24 -0400

    update

--- scripts/helm_deploy.sh
+++ scripts/helm_deploy.sh
@@ -5,10 +5,14 @@ set -e
 HELM_RELEASE_NAME="react-web-app"
 HELM_CHART_PATH="helm/react-web-app-chart"
 KUBE_NAMESPACE="default"
+KUBECONFIG_PATH="/path/to/your/kubeconfig"
+
+# Set KUBECONFIG environment variable
+export KUBECONFIG=$KUBECONFIG_PATH
 
 # Deploy the Helm chart
 echo "Deploying Helm chart..."
-helm upgrade --install $HELM_RELEASE_NAME $HELM_CHART_PATH --namespace $KUBE_NAMESPACE
+helm upgrade --install $HELM_RELEASE_NAME $HELM_CHART_PATH --namespace $KUBE_NAMESPACE --kubeconfig $KUBECONFIG_PATH
 
 # Verify deployment
 helm status $HELM_RELEASE_NAME
